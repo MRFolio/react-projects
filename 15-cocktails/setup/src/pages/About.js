a21c42c0e1690d9e35fbe96622f09c924368ecec
MRFolio <roose.markus@gmail.com>
2020-11-19 23:45:55 +0200

    feat(cocktails): add most of the setup and routing

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum quaerat
+        fugit officia, omnis repellendus ipsam? Iusto illo quas quidem nobis
+        quaerat, sint dolorem unde excepturi, nisi recusandae aliquam, earum
+        animi minima commodi facilis. Dolorum et similique ipsam architecto
+        velit expedita.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
